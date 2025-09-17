7e5c9cf2cb7d53dd2140f57b87c55317c01adb30
MHammad-374 <mhammadif374@gmail.com>
2025-09-18 01:20:36 +0500

    Add routes

--- controllers/users.js
+++ controllers/users.js
@@ -16,64 +16,40 @@ const getUsers = async (req, res) => {
 const delUsers = async (req, res) => {
     try {
         const { id } = req.params;
+
         const deletedUser = await Users.findByIdAndDelete(id);
-        if (!deletedUser) {
+
+        if (!deletedUser)
             return res.status(404).json({ message: "User not found" });
-        }
+
         res.json({ message: "User deleted successfully", user: deletedUser });
+
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error" })
     }
 }
 
-// const createUser = async (req, res) => {
-//     try {
-//         // const { name } = req.body;
-//         // if (!name) {
-//         //     return res.status(400).json({ message: "Name is required" });
-//         // }
-//         const newUser = new Users({ name: "name" })
-//         await newUser.save()
-//             .then(() => {
-//                 res.status(201).json({
-//                     message: "User created successfully",
-//                     user: newUser,
-//                 });
-//             })
-//             .catch((err) => {
-//                     return res.status(400).json({
-//                         msg: "Internal Server Error",
-//                         error: err.message
-//                     });
-//                 })
-//     } catch (error) {
-//         res.status(500).json({ message: "Internal Server Error" })
-//     }
-// }
-
 const createUser = async (req, res) => {
-  try {
-    const { name } = req.body;
+    try {
+        const { name } = req.body;
 
-    if (!name) {
-      return res.status(400).json({ message: "Name is required" });
+        if (!name) return res.status(400).json({ message: "Name is required" });
+
+        const newUser = new Users({ name });
+
+        await newUser.save();
+
+        res.status(201).json({
+            message: "User created successfully",
+            user: newUser,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: error.message,
+        });
     }
-
-    const newUser = new Users({ name });
-
-    await newUser.save();
-
-    res.status(201).json({
-      message: "User created successfully",
-      user: newUser,
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      message: "Internal Server Error",
-      error: error.message,
-    });
-  }
 };
 
 
